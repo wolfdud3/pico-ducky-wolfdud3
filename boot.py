f39b3b4f581879595f6f60e6a8bba30bfec3553e
wolfdud3 <45672884+wolfdud3@users.noreply.github.com>
2023-01-26 18:24:04 +0200

    Add files via upload

---
+++ boot.py
@@ -0,0 +1,12 @@
+from board import *
+import digitalio
+import storage
+
+noStorageStatus = False
+noStoragePin = digitalio.DigitalInOut(GP16)
+noStoragePin.switch_to_input(pull=digitalio.Pull.UP)
+noStorageStatus = not noStoragePin.value
+
+if(noStorageStatus == True):
+    # don't show USB drive to host PC
+    storage.disable_usb_drive()
